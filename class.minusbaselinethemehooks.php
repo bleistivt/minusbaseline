4e092b21763a8c0bcc1418351b0c8a01dacd06c0
bleistivt <nmh.acww@yahoo.de>
2014-10-03 15:56:18 +0200

    more code

--- class.minusbaselinethemehooks.php
+++ class.minusbaselinethemehooks.php
@@ -4,31 +4,43 @@ class MinusBaseLineThemeHooks implements Gdn_IPlugin {
 
 	public function Setup() { }
 
+	//remove mobile-unfriendly plugins
 	public function Gdn_Dispatcher_AfterAnalyzeRequest_Handler($Sender) {
 		if (in_array($Sender->Application(), array('vanilla', 'conversations')) || ($Sender->Application() == 'dashboard' && in_array($Sender->Controller(), array('Activity', 'Profile', 'Search')))) {
-		 Gdn::PluginManager()->RemoveMobileUnfriendlyPlugins();
+			Gdn::PluginManager()->RemoveMobileUnfriendlyPlugins();
 		}
 		SaveToConfig('Garden.Format.EmbedSize', '240x135', false);
 	}
 
 	public function Base_Render_Before($Sender) {
+		//add the hamburger menu
 		$Sender->AddAsset('Content', Anchor('n', Url('#'), 'Hamburger'), 'Hamburger');
 
+		//the js file contains the code for the menu and a better way to make discussion clickable
 		$Sender->AddJsFile('minusbaseline.js');
 
+		//add the searchbox to the panel
 		//copied from library/vendors/SmartyPlugins/function.searchbox.php
 		$Form = Gdn::Factory('Form');
 		$Form->InputPrefix = '';
 		$Result =
-		$Form->Open(array('action' => Url('/search'), 'method' => 'get')).
-		$Form->TextBox('Search', array('placeholder' => T('SearchBoxPlaceHolder', 'Search'))).
-		$Form->Button('Go', array('Name' => '')).
-		$Form->Close();
+			$Form->Open(array('action' => Url('/search'), 'method' => 'get')).
+			$Form->TextBox('Search', array('placeholder' => T('SearchBoxPlaceHolder', 'Search'))).
+			$Form->Button('Go', array('Name' => '')).
+			$Form->Close();
 		$Result = Wrap($Result, 'div', array('class' => 'SiteSearch'));
-
 		$Sender->AddAsset('Panel', $Result, 'SearchBox');
+		
+		//nomobile link to switch to the full site
+		$NoMobile = Gdn_Theme::Link(
+			'profile/nomobile',
+			T("Full Site"),
+			Wrap('<a href="%url" class="%class">%text</a>', 'div', array('class' => 'NoMobile'))
+		);
+		$Sender->AddAsset('Foot', $NoMobile, 'NoMobile');
 	}
 
+	//disable admincheckboxes and option cogwheels
 	public function CategoriesController_Render_Before($Sender) {
 		$Sender->ShowOptions = false;
 		SaveToConfig('Vanilla.AdminCheckboxes.Use', false, false);
@@ -39,8 +51,10 @@ class MinusBaseLineThemeHooks implements Gdn_IPlugin {
 		SaveToConfig('Vanilla.AdminCheckboxes.Use', false, false);
 	}
 
+	//put the userphoto in the content area of profiles
 	public function ProfileController_BeforeUserInfo_Handler($Sender) {
 		$UserPhoto = new UserPhotoModule();
 		echo $UserPhoto->ToString();
 	}
+
 }
--- js/minusbaseline.js
+++ js/minusbaseline.js
@@ -1,17 +1,18 @@
 jQuery(function($) {
 
+	//toggle the menuopen class when the hamburger is clicked
 	$('a.Hamburger').click(function(e) {
 		e.preventDefault();
 		$('body').toggleClass('HamburgerOpen');
 	})
 	
+	//make the whole area of DiscussionList Items clickable
 	$('ul.DataList.Discussions, ul.DataList.Itemlisten').on('click', 'li.Item', function (e) {
         var href = $(this).find('.Title a').attr('href');
         if (!$(e.target).is('span.OptionsTitle, a, a.Bookmark, input') && typeof href !== 'undefined') {
             document.location = href;
         }
     });
-	
     $('div.MeMenu').on('click', 'li.Item', function () {
         var href = $(this).find('a:last').attr('href');
         if (typeof href !== 'undefined') {
