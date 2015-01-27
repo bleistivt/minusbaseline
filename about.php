a6cd78ccd0ada337b94b4fa815838d5204c6d19a
bleistivt <bleistivt@users.noreply.github.com>
2015-01-27 17:39:28 +0100

    Add license info

--- about.php
+++ about.php
@@ -1,10 +1,11 @@
 <?php if (!defined('APPLICATION')) exit();
 
 $ThemeInfo['minusbaseline'] = array(
-	'Name' => 'MinusBaseline',
-	'Description' => 'The default theme with a few customizations to work better on mobile devices.',
-	'Version' => '0.5',
-	'Author' => 'Bleistivt',
-	'AuthorUrl' => 'http://bleistivt.net',
-	'IsMobile' => true
+    'Name' => 'MinusBaseline',
+    'Description' => 'The default theme with a few customizations to work better on mobile devices.',
+    'Version' => '0.5',
+    'Author' => 'Bleistivt',
+    'AuthorUrl' => 'http://bleistivt.net',
+    'License' => 'GNU GPL2',
+    'IsMobile' => true
 );
