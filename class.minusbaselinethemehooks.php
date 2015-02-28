0dae54d40a343f98b4066a89316aed64be3e3054
bleistivt <bleistivt@users.noreply.github.com>
2015-02-28 07:54:47 +0100

    tabs to spaces

--- class.minusbaselinethemehooks.php
+++ class.minusbaselinethemehooks.php
@@ -2,62 +2,62 @@
 
 class MinusBaseLineThemeHooks implements Gdn_IPlugin {
 
-	public function Setup() { }
+    public function Setup() {}
 
-	//remove mobile-unfriendly plugins
-	public function Gdn_Dispatcher_AfterAnalyzeRequest_Handler($Sender) {
-		if (in_array($Sender->Application(), array('vanilla', 'conversations')) || ($Sender->Application() == 'dashboard' && in_array($Sender->Controller(), array('Activity', 'Profile', 'Search')))) {
-			Gdn::PluginManager()->RemoveMobileUnfriendlyPlugins();
-		}
-		SaveToConfig('Garden.Format.EmbedSize', '240x135', false);
+    //remove mobile-unfriendly plugins
+    public function Gdn_Dispatcher_AfterAnalyzeRequest_Handler($Sender) {
+        if (in_array($Sender->Application(), array('vanilla', 'conversations')) || ($Sender->Application() == 'dashboard' && in_array($Sender->Controller(), array('Activity', 'Profile', 'Search')))) {
+            Gdn::PluginManager()->RemoveMobileUnfriendlyPlugins();
+        }
+        SaveToConfig('Garden.Format.EmbedSize', '240x135', false);
 
-		//the table discussions layout takes up too much space on small screens
-		SaveToConfig('Vanilla.Discussions.Layout', 'modern', false);
-	}
+        //the table discussions layout takes up too much space on small screens
+        SaveToConfig('Vanilla.Discussions.Layout', 'modern', false);
+    }
 
-	public function Base_Render_Before($Sender) {
-		if ($Sender->MasterView == 'admin') return;
+    public function Base_Render_Before($Sender) {
+        if ($Sender->MasterView == 'admin') return;
 
-		//tell the browser this is a mobile style
-		$Sender->Head->AddTag('meta', array('name' => 'viewport', 'content' => "width=device-width,minimum-scale=1.0,maximum-scale=1.0"));
-		
-		//add the hamburger menu
-		$Sender->AddAsset('Content', Anchor('n', Url('#'), 'Hamburger'), 'Hamburger');
+        //tell the browser this is a mobile style
+        $Sender->Head->AddTag('meta', array('name' => 'viewport', 'content' => "width=device-width,minimum-scale=1.0,maximum-scale=1.0"));
 
-		//add the searchbox to the panel
-		//copied from library/vendors/SmartyPlugins/function.searchbox.php
-		$Form = Gdn::Factory('Form');
-		$Form->InputPrefix = '';
-		$Result =
-			$Form->Open(array('action' => Url('/search'), 'method' => 'get')).
-			$Form->TextBox('Search', array('placeholder' => T('SearchBoxPlaceHolder', 'Search'))).
-			$Form->Button('Go', array('Name' => '')).
-			$Form->Close();
-		$Result = Wrap($Result, 'div', array('class' => 'SiteSearch'));
-		$Sender->AddAsset('Panel', $Result, 'SearchBox');
-		
-		//nomobile link to switch to the full site
-		$NoMobile = Gdn_Theme::Link(
-			'profile/nomobile',
-			T("Full Site"),
-			Wrap('<a href="%url" class="%class">%text</a>', 'div', array('class' => 'NoMobile'))
-		);
-		$Sender->AddAsset('Foot', $NoMobile, 'NoMobile');
-	}
+        //add the hamburger menu
+        $Sender->AddAsset('Content', Anchor('n', Url('#'), 'Hamburger'), 'Hamburger');
 
-	//disable admincheckboxes
-	public function CategoriesController_Render_Before($Sender) {
-		SaveToConfig('Vanilla.AdminCheckboxes.Use', false, false);
-	}
+        //add the searchbox to the panel
+        //copied from library/vendors/SmartyPlugins/function.searchbox.php
+        $Form = Gdn::Factory('Form');
+        $Form->InputPrefix = '';
+        $Result =
+            $Form->Open(array('action' => Url('/search'), 'method' => 'get')).
+            $Form->TextBox('Search', array('placeholder' => T('SearchBoxPlaceHolder', 'Search'))).
+            $Form->Button('Go', array('Name' => '')).
+            $Form->Close();
+        $Result = Wrap($Result, 'div', array('class' => 'SiteSearch'));
+        $Sender->AddAsset('Panel', $Result, 'SearchBox');
 
-	public function DiscussionsController_Render_Before($Sender) {
-		SaveToConfig('Vanilla.AdminCheckboxes.Use', false, false);
-	}
+        //nomobile link to switch to the full site
+        $NoMobile = Gdn_Theme::Link(
+            'profile/nomobile',
+            T("Full Site"),
+            Wrap('<a href="%url" class="%class">%text</a>', 'div', array('class' => 'NoMobile'))
+        );
+        $Sender->AddAsset('Foot', $NoMobile, 'NoMobile');
+    }
 
-	//put the userphoto in the content area of profiles
-	public function ProfileController_BeforeUserInfo_Handler($Sender) {
-		$UserPhoto = new UserPhotoModule();
-		echo $UserPhoto->ToString();
-	}
+    //disable admincheckboxes
+    public function CategoriesController_Render_Before($Sender) {
+        SaveToConfig('Vanilla.AdminCheckboxes.Use', false, false);
+    }
+
+    public function DiscussionsController_Render_Before($Sender) {
+        SaveToConfig('Vanilla.AdminCheckboxes.Use', false, false);
+    }
+
+    //put the userphoto in the content area of profiles
+    public function ProfileController_BeforeUserInfo_Handler($Sender) {
+        $UserPhoto = new UserPhotoModule();
+        echo $UserPhoto->ToString();
+    }
 
 }
