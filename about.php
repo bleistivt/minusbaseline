7666ffd0afa37ceb3227b56532507c4ba5dccd0c
bleistivt <bleistivt@users.noreply.github.com>
2015-05-02 15:56:06 +0200

    version bump

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['minusbaseline'] = array(
     'Name' => 'MinusBaseline',
     'Description' => 'The default theme with a few customizations to work better on mobile devices.',
-    'Version' => '0.6',
+    'Version' => '0.7',
     'Author' => 'Bleistivt',
     'AuthorUrl' => 'http://bleistivt.net',
     'License' => 'GNU GPL2',
